ec21382d11b06e680cc24360dcf88be9025e59f9
Emerentius <Emerentius@arcor.de>
2017-10-25 03:30:16 +0200

    impl OrdSubset for all primitive types
    until the necessary specialization features hit
    slices up to arity of 12, arays to 32 like the std library

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,9 +21,15 @@ but no memory unsafety
 
 Version 3.0.0 (2017-10-21)
 ==========================
-* Add `sort_unstable*` variants, following the std library. Technically a [breaking change].
+* Add `sort_unstable*` variants, following the std library additions in 1.20. Technically a [breaking change].
 * Generalized traits so that `.ord_subset_binary_search*` works on immutable slices and anything else that implements AsRef<[T]>, but not AsMut<[T]>. Fixing this major oversight is also technically a [breaking change].
-* #![no_std] support with new opt-out feature "std"
-  Stable sorts and OrdSubset implementations for f32 and f64 are unavailable in no_std mode 
-  The latter can be regained but this depends on the unstable "core_float" feature gate (as of 2017-09-20, rustc 1.22), opt in via this crate's "unstable" feature
+* `#![no_std]` support with new opt-out feature `std`
+  Stable sorts are unavailable in `no_std` mode.
+  OrdSubset implementations for f32, f64 with `no_std` require the `unstable` feature of this crate (impls readded in 3.1 without `unstable`).
 * Stabilized `_rev()` variants
+
+Unreleased
+==========================
+* Overload ops for `OrdVar` to work on anything which works for the contained type, checking each result for order. Added by new default feature `ops` with opt-in feature `unchecked_ops`.
+* Add `OrdSubset` impls for `f32`, `f64` in `no_std` mode without requiring `unstable`.
+* Add `OrdSubset` impls for all `Ord` intrinsics
--- src/ord_subset_trait.rs
+++ src/ord_subset_trait.rs
@@ -16,7 +16,7 @@ pub trait OrdSubset: PartialOrd<Self> + PartialEq<Self> {
 impl<'a, A> OrdSubset for &'a A where A: OrdSubset {
 	#[inline(always)]
 	fn is_outside_order(&self) -> bool {
-		(*self).is_outside_order()
+		(**self).is_outside_order()
 	}
 }
 
@@ -41,6 +41,187 @@ impl OrdSubset for f32 {
 		// only NaNs != itself
 		*self != *self
 	}
+}
+
+trait EnsureOrd: Ord {}
+
+macro_rules! impl_for_ord {
+	($($type:ty),+) => (
+		$(	
+			// safe guard against incorrect macro invocation
+			// `where Self: Ord` on OrdSubset impl would be rendered in docs
+			impl EnsureOrd for $type {} 
+			
+			impl OrdSubset for $type
+			{
+				#[inline(always)]
+				fn is_outside_order(&self) -> bool {
+					false
+				}
+			}
+		)+
+	)
+}
+
+impl_for_ord!(u8, u16, u32, u64, usize, i8, i16, i32, i64, isize, bool, char);
+
+macro_rules! array_impls {
+    ($($N:expr),+) => {
+        $(
+			impl<T: OrdSubset> OrdSubset for [T; $N] {
+				#[inline(always)]
+				fn is_outside_order(&self) -> bool {
+					(&self[..]).is_outside_order()
+				}
+			}
+        )+
+    }
+}
+
+array_impls!(
+	0, 1, 2, 3, 4, 5, 6, 7, 8,
+	9, 10, 11, 12, 13, 14, 15, 16,
+	17, 18, 19, 20, 21, 22, 23, 24,
+	25, 26, 27, 28, 29, 30, 31, 32
+);
+
+impl<T: OrdSubset> OrdSubset for [T] {
+	#[inline(always)]
+	fn is_outside_order(&self) -> bool {
+		self.iter().any(OrdSubset::is_outside_order)
+	}
+}
+
+impl OrdSubset for () {
+	fn is_outside_order(&self) -> bool { false } // or true, whatever
+}
+
+// code stolen from std library
+macro_rules! tuple_impls {
+    ($(
+        $Tuple:ident {
+            $(($idx:tt) -> $T:ident)+
+        }
+    )+) => {
+        $(
+            impl<$($T:OrdSubset),+> OrdSubset for ($($T,)+) where last_type!($($T,)+): ?Sized {
+                #[inline]
+                fn is_outside_order(&self) -> bool {
+                    $(self.$idx.is_outside_order())||+
+                }
+            }
+        )+
+    }
+}
+
+macro_rules! last_type {
+    ($a:ident,) => { $a };
+    ($a:ident, $($rest_a:ident,)+) => { last_type!($($rest_a,)+) };
+}
+
+tuple_impls! {
+    Tuple1 {
+        (0) -> A
+    }
+    Tuple2 {
+        (0) -> A
+        (1) -> B
+    }
+    Tuple3 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+    }
+    Tuple4 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+    }
+    Tuple5 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+    }
+    Tuple6 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+    }
+    Tuple7 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+    }
+    Tuple8 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+    }
+    Tuple9 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+    }
+    Tuple10 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+    }
+    Tuple11 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+        (10) -> K
+    }
+    Tuple12 {
+        (0) -> A
+        (1) -> B
+        (2) -> C
+        (3) -> D
+        (4) -> E
+        (5) -> F
+        (6) -> G
+        (7) -> H
+        (8) -> I
+        (9) -> J
+        (10) -> K
+        (11) -> L
+    }
 }
 
 // Small helper used a lot in sorts
